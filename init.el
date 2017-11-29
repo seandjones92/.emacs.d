6deaaed30cf81ebb89887c97110291e5a5adb771
Sean Jones <sean.d.jones92@gmail.com>
2017-11-28 18:19:47 -0700

    python hook linum mode

--- init.el
+++ init.el
@@ -176,6 +176,7 @@ This will first empty the kill-ring (clipboard)"
 (add-hook 'text-mode-hook 'toggle-truncate-lines)
 (add-hook 'org-mode-hook 'turn-on-font-lock)
 (add-hook 'sh-mode-hook 'linum-mode)
+(add-hook 'python-mode-hook 'linum-mode)
 
 ;;;; Custom keybindings ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -274,3 +275,17 @@ This will first empty the kill-ring (clipboard)"
 
 (if (require 'moe-theme)
     (my-moetheme-setup))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (multiple-cursors moe-theme markdown-mode magit hlinum helm-projectile csv-nav auto-complete))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
